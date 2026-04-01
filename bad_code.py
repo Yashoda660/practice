e12b4354f4c7e8cc04e6ebe49e138b60781475ef
BhuvanChandra26 <bhuvanchandra2611@gmail.com>
2026-04-01 14:32:01 +0530

    Add python file for SonarQube analysis

---
+++ bad_code.py
@@ -0,0 +1,11 @@
+# bad_code.py
+
+def divide(a, b):
+    return a / b   # Possible ZeroDivisionError
+
+def unused_function():
+    x = 10
+    y = 20
+    return x + y
+
+password = "admin123"  # Hardcoded password (security issue)
