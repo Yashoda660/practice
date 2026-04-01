979a2c191da4d684877fe8bb822e273389fa8bb0
BhuvanChandra26 <bhuvanchandra2611@gmail.com>
2026-04-01 17:23:50 +0530

    Add buggy python code to demonstrate SonarQube bugs

--- hello.py
+++ hello.py
@@ -1,2 +1,15 @@
-import os
-password = os.getenv("APP_PASSWORD")
+# hello.py
+
+def divide(a, b):
+    return a / b        # BUG: division by zero possible
+
+def list_access():
+    numbers = [1, 2, 3]
+    return numbers[10] # BUG: index out of range
+
+def undefined_usage():
+    print(user_name)   # BUG: undefined variable
+
+divide(10, 0)
+list_access()
+undefined_usage()
